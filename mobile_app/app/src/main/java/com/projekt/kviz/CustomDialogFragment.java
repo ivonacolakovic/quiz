0cd02f05dffb1e4daac35561ef165ef5892ac8c7
Verica <verica.zagoranski@tridens.si>
2019-01-20 01:26:16 +0100

    [edit] show questions activity

---
+++ mobile_app/app/src/main/java/com/projekt/kviz/CustomDialogFragment.java
@@ -0,0 +1,28 @@
+package com.projekt.kviz;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+public class CustomDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(final Bundle savedInstanceState) {
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        Bundle bundle = getArguments();
+        String odgovor = bundle.getString("odgovor", "");
+        builder.setMessage(odgovor)
+                .setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        return builder.create();
+
+    }
+}
--- mobile_app/app/src/main/java/com/projekt/kviz/ShowQuestionsActivity.java
+++ mobile_app/app/src/main/java/com/projekt/kviz/ShowQuestionsActivity.java
@@ -1,15 +1,26 @@
 package com.projekt.kviz;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.Console;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -21,41 +32,207 @@ import java.util.Random;
 public class ShowQuestionsActivity extends AppCompatActivity {
 
     // public final String url = "https://opentdb.com/api.php?amount=50";
-    //public final String url = "http://192.168.1.107:5555/";
     TextView showData;
     ArrayList<Question> list;
-    ArrayList<Question> easyQ;
-    ArrayList<Question> mediumQ;
-    ArrayList<Question> hardQ;
-    TextView answer1;
-    TextView answer2;
-    TextView answer3;
-    TextView answer4;
+    JSONArray easyQ;
+    JSONArray mediumQ;
+    JSONArray hardQ;
+    CheckBox answer1;
+    CheckBox answer2;
+    CheckBox answer3;
+    CheckBox answer4;
     String[] showAnswers;
+    ImageButton help;
+    int numberOfCoins = 0;
+    int level = 1;
+    JSONObject questions = null;
+    String pravilenOdgovor;
+    Button next;
+    TextView coin;
+    JSONObject vprasanjaZaEnNivo = null;
+
+    int steviloPravilnihOdgovorov = 0;
+    int steviloNapacnihOdgovorov = 0;
+    int stevecZaForZanke = 0; //ta stevec se poveca samo, ko uporabnik klikne na gumb next in,
+                              // če je izbral en od odgovorv - če ni izbral nobenega od ponujenih
+                              //odgovorov potem se izpise toast z opozorilom, da izbere pravilni odgovor
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_questions_activity);
 
+        help = findViewById(R.id.imageButton2);
         showData = findViewById(R.id.showData);
         answer1 = findViewById(R.id.answer1);
         answer2 = findViewById(R.id.answer2);
         answer3 = findViewById(R.id.answer3);
         answer4 = findViewById(R.id.answer4);
-        //showAnswers = new String[] {"answer1", "answer2", "answer3", "answer4"};
-        //showData.setText(QuestionList.questionList.get(1).getQuestion());
+        next = findViewById(R.id.nextButton);
+        coin = findViewById(R.id.coin);
+
+        String url = "http://164.8.207.4:5555/api/game/regular/get_questions";
+        new DownloadDataTask(url).execute();
+       // if(message.equals("izbranLevel")){
+            //TODO: DODAJ TU, ČE UPORABNIK IZBERE SAMO EN LEVEL
+            //TODO: DODAJ, DA SE POVEČA COIN IN DA SE SPREMENI TEXTVIEW ZA COIN
+            //TODO: KOD FOR ZANKE PRI LISTANJU PITANJA I ODGOVORA STAVI DA SE I POVEĆA TEK DOK JE GUMB KLIKNUTI, AKO JE VISE LEVELA ONDA SE IZMEĐU LEVELA TREBA PROMIJENITI V NULU OPET
+
+        //}else{
+            try {
+                easyQ = questions.getJSONArray("easy");
+                mediumQ = questions.getJSONArray("medium");
+                hardQ = questions.getJSONArray("hard");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            for(; stevecZaForZanke<easyQ.length();){
+                ArrayList<String> shuffled = new ArrayList<>();
+
+                try {
+                    showData.setText(easyQ.getJSONObject(stevecZaForZanke).getString("vprasanje"));
+                    pravilenOdgovor = easyQ.getJSONObject(stevecZaForZanke).getString("pravilen_odgovor");
+                    shuffleArray(shuffled, easyQ, stevecZaForZanke);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                answer1.setText(shuffled.get(0));
+                answer2.setText(shuffled.get(1));
+                answer3.setText(shuffled.get(2));
+                answer4.setText(shuffled.get(3));
+            }
+
+            // if, ki preveri ali uporabnik lahko gre na naslednji nivo - če je število pravilnih enako 9
+            if(steviloPravilnihOdgovorov == 10){
+                stevecZaForZanke = 0;
+                steviloNapacnihOdgovorov = 0;
+                steviloPravilnihOdgovorov = 0;
+                level = 2; //povečamo, da pri kliku na gumb, če je uporabnik pravilo odgovoril dobi večje število kovancev na višjem nivoju
+                for(; stevecZaForZanke<mediumQ.length();){
+                    ArrayList<String> shuffled = new ArrayList<>();
+
+                    try {
+                        showData.setText(mediumQ.getJSONObject(stevecZaForZanke).getString("vprasanje"));
+                        pravilenOdgovor = mediumQ.getJSONObject(stevecZaForZanke).getString("pravilen_odgovor");
+                        shuffleArray(shuffled, mediumQ, stevecZaForZanke);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    answer1.setText(shuffled.get(0));
+                    answer2.setText(shuffled.get(1));
+                    answer3.setText(shuffled.get(2));
+                    answer4.setText(shuffled.get(3));
+                }
+
+            }
+
+            if(steviloPravilnihOdgovorov == 10){
+                stevecZaForZanke = 0;
+                steviloNapacnihOdgovorov = 0;
+                steviloPravilnihOdgovorov = 0;
+                level = 3; //povečamo, da pri kliku na gumb, če je uporabnik pravilo odgovoril dobi večje število kovancev na višjem nivoju
+                for(; stevecZaForZanke<hardQ.length();){
+                    ArrayList<String> shuffled = new ArrayList<>();
+
+                    try {
+                        showData.setText(hardQ.getJSONObject(stevecZaForZanke).getString("vprasanje"));
+                        pravilenOdgovor = hardQ.getJSONObject(stevecZaForZanke).getString("pravilen_odgovor");
+                        shuffleArray(shuffled, hardQ, stevecZaForZanke);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    answer1.setText(shuffled.get(0));
+                    answer2.setText(shuffled.get(1));
+                    answer3.setText(shuffled.get(2));
+                    answer4.setText(shuffled.get(3));
+                }
+
+            }
+
+      //  }
+
+
+
+        //implementacija gumba
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String odgovor = "";
+                if(answer1.isChecked()==false && answer2.isChecked()==false && answer3.isChecked()==false && answer4.isChecked()==false){
+                    odgovor = "Izberi enega izmed ponujenih odgovorov!";
+                }else if((answer1.isChecked() && answer1.getText()==pravilenOdgovor) || (answer2.isChecked() && answer2.getText()==pravilenOdgovor) ||
+                        (answer3.isChecked() && answer3.getText()==pravilenOdgovor) || (answer4.isChecked() && answer4.getText()==pravilenOdgovor)){
+                    odgovor = "Odgovor je pravilen!";
+                    stevecZaForZanke++;
+                    steviloPravilnihOdgovorov++;
+
+                    if(level == 1){
+                        numberOfCoins = numberOfCoins + 5;
+                    }else if(level == 2){
+                        numberOfCoins = numberOfCoins + 10;
+                    }else{
+                        numberOfCoins = numberOfCoins + 15;
+                    }
+
+                }else if((answer1.isChecked() && answer1.getText()!=pravilenOdgovor) || (answer2.isChecked() && answer2.getText()!=pravilenOdgovor) ||
+                        (answer3.isChecked() && answer3.getText()!=pravilenOdgovor) || (answer4.isChecked() && answer4.getText()!=pravilenOdgovor)){
+                    odgovor = "Odgovor je napačen. Pravilen odgovor: " + pravilenOdgovor;
+                    stevecZaForZanke++;
+                    steviloNapacnihOdgovorov++;
+                }
+
+                Toast.makeText(getApplicationContext(), odgovor,
+                        Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+
+            help.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String odgovor = "Pravilen odgovor je: " + pravilenOdgovor;
+                    if(numberOfCoins <5){
+                        odgovor = "Ni dovolj kovančkov!";
+                    }
+                    FragmentManager fm = getSupportFragmentManager();
+
+                    CustomDialogFragment newFragment = new CustomDialogFragment();
+
+                    Bundle bundle = new Bundle();
+                    bundle.putString("odgovor", odgovor);
+
+                    newFragment.setArguments(bundle);
+                    newFragment.show(fm, "custom_dialog");
+                }
+            });
+
+
 
     }
+    public void shuffleArray(ArrayList<String> array, JSONArray data, int position) throws JSONException {
+        array.add(data.getJSONObject(position).getString("pravilen_odgovor"));
+        array.add(data.getJSONObject(position).getString("nepravilen_odgovor1"));
+        array.add(data.getJSONObject(position).getString("nepravilen_odgovor2"));
+        array.add(data.getJSONObject(position).getString("nepravilen_odgovor3"));
 
+        Collections.shuffle(array);
+
+    }
+/*
     @Override
     protected void onStart() {
         super.onStart();
-        //get url for connection with backend
-        Intent i = getIntent();
-        String url = i.getStringExtra("url");
-        new DownloadDataTask(url).execute(url);
+        //String url = getIntent().getStringExtra("url");
+        new DownloadDataTask(url).execute();
     }
+    */
 
     private class DownloadDataTask extends AsyncTask<String, Void, String> {
 
@@ -69,35 +246,11 @@ public class ShowQuestionsActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            //new ParserTask().execute(s);
-                //call parsing method
-                /*easyQ = new ArrayList<>();
-                mediumQ = new ArrayList<>();
-                hardQ = new ArrayList<>();
-                list = parser.getQuestionList();
-                for(Question qst: list){
-                    if (qst.getDifficulty().equals("easy")){
-                        easyQ.add(qst);
-                    } else if (qst.getDifficulty().equals("medium")){
-                        mediumQ.add(qst);
-                    } else if (qst.getDifficulty().equals("hard")){
-                        hardQ.add(qst);
-                    }
-                }
-                //int level = 0;
-                //int rand = new Random().nextInt(4);
-                Log.d("SIZE", "EasyQ size: "+easyQ.size());
-                Question selected = easyQ.get(new Random().nextInt(easyQ.size()));
-                Log.d("SELECTED", "Selected question: "+selected.getQuestion());
-                showData.setText(selected.getQuestion());
-                ArrayList<String> finalAnswers = selected.getIncorrectAnswers();
-                finalAnswers.add(selected.getCorrectAnswer());
-                Collections.shuffle(finalAnswers);
-                answer1.setText(finalAnswers.get(0));
-                answer2.setText(finalAnswers.get(1));
-                answer3.setText(finalAnswers.get(2));
-                answer4.setText(finalAnswers.get(3));*/
-
+            try {
+                questions = new JSONObject(s);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
@@ -110,7 +263,7 @@ public class ShowQuestionsActivity extends AppCompatActivity {
             }
         }
 
-        private String downloadFromUrl(String urlString) throws IOException {
+        private String downloadFromUrl(String urlString) throws IOException{
             // get connection
             java.net.URL url = new java.net.URL(urlString);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -122,11 +275,15 @@ public class ShowQuestionsActivity extends AppCompatActivity {
             conn.connect();
 
             // get data from database
+            Log.d("SELECTED", "PRIJE");
             InputStream stream = conn.getInputStream();
+
             BufferedReader buffer = new BufferedReader(new InputStreamReader(stream));
             String result = buffer.readLine();
             Log.d("DOWNLOADED: ", result);
             return result;
         }
     }
+
+
 }
